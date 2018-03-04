60a8c15a990f1219baee8fbb3128a636ad09ab26
looko <31150545+lookoutldz@users.noreply.github.com>
2018-03-04 10:06:00 +0800

    add actuator

--- src/main/java/looko/looksteam/demo/api/GetPlayerSummaries.java
+++ src/main/java/looko/looksteam/demo/api/GetPlayerSummaries.java
@@ -1,8 +1,7 @@
 package looko.looksteam.demo.api;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+
+import com.google.gson.*;
 import looko.looksteam.demo.entity.Player;
 import looko.looksteam.demo.tool.GetNowTime;
 import looko.looksteam.demo.tool.X;
@@ -26,45 +25,50 @@ public class GetPlayerSummaries {
         Player player = new Player();
         InputStreamReader isr = new InputStreamReader(is);
 
-        JsonParser parser = new JsonParser();
-        JsonObject root = parser.parse(isr).getAsJsonObject();
-        JsonObject responseObj = root.get("response").getAsJsonObject();
-        JsonArray players = responseObj.get("players").getAsJsonArray();
-        JsonObject result = players.get(0).getAsJsonObject();
+        try {
+            JsonParser parser = new JsonParser();
+            JsonObject root = parser.parse(isr).getAsJsonObject();
+            JsonObject responseObj = root.get("response").getAsJsonObject();
+            JsonArray players = responseObj.get("players").getAsJsonArray();
+            JsonObject result = players.get(0).getAsJsonObject();
 
-        player.setSteamid(result.get("steamid").getAsString());
-        player.setCommunityvisibilitystate(result.get("communityvisibilitystate").getAsInt());
-        player.setProfilestate(result.get("profilestate").getAsInt());
-        player.setPersonaname(result.get("personaname").getAsString());
-        player.setLastlogoff(result.get("lastlogoff").getAsInt());
-        if (result.has("commentpermission"))
-            player.setCommentpermission(result.get("commentpermission").getAsInt());
-        player.setProfileurl(result.get("profileurl").getAsString());
-        player.setAvatar(result.get("avatar").getAsString());
-        player.setAvatarmedium(result.get("avatarmedium").getAsString());
-        player.setAvatarfull(result.get("avatarfull").getAsString());
-        player.setPersonastate(result.get("personastate").getAsInt());
-        player.setTimecreated(result.get("timecreated").getAsInt());
-        player.setPersonastateflags(result.get("personastateflags").getAsInt());
+            player.setSteamid(result.get("steamid").getAsString());
+            player.setCommunityvisibilitystate(result.get("communityvisibilitystate").getAsInt());
+            player.setProfilestate(result.get("profilestate").getAsInt());
+            player.setPersonaname(result.get("personaname").getAsString());
+            player.setLastlogoff(result.get("lastlogoff").getAsInt());
+            if (result.has("commentpermission"))
+                player.setCommentpermission(result.get("commentpermission").getAsInt());
+            player.setProfileurl(result.get("profileurl").getAsString());
+            player.setAvatar(result.get("avatar").getAsString());
+            player.setAvatarmedium(result.get("avatarmedium").getAsString());
+            player.setAvatarfull(result.get("avatarfull").getAsString());
+            player.setPersonastate(result.get("personastate").getAsInt());
+            player.setTimecreated(result.get("timecreated").getAsInt());
+            player.setPersonastateflags(result.get("personastateflags").getAsInt());
 
-        if (result.has("realname"))
-            player.setRealname(result.get("realname").getAsString());
-        if (result.has("primaryclanid"))
-            player.setPrimaryclanid(result.get("primaryclanid").getAsString());
-        if (result.has("gameextrainfo"))
-            player.setGameextrainfo(result.get("gameextrainfo").getAsString());
-        if (result.has("gameserverip"))
-            player.setGameserverip(result.get("gameserverip").getAsString());
-        if (result.has("gameid"))
-            player.setGameid(result.get("gameid").getAsInt());
-        if (result.has("loccountrycode"))
-            player.setLoccountrycode(result.get("loccountrycode").getAsString());
-        if (result.has("locstatecode"))
-            player.setLocstatecode(result.get("locstatecode").getAsString());
-        if (result.has("loccityid"))
-            player.setLoccityid(result.get("loccityid").getAsString());
+            if (result.has("realname"))
+                player.setRealname(result.get("realname").getAsString());
+            if (result.has("primaryclanid"))
+                player.setPrimaryclanid(result.get("primaryclanid").getAsString());
+            if (result.has("gameextrainfo"))
+                player.setGameextrainfo(result.get("gameextrainfo").getAsString());
+            if (result.has("gameserverip"))
+                player.setGameserverip(result.get("gameserverip").getAsString());
+            if (result.has("gameid"))
+                player.setGameid(result.get("gameid").getAsInt());
+            if (result.has("loccountrycode"))
+                player.setLoccountrycode(result.get("loccountrycode").getAsString());
+            if (result.has("locstatecode"))
+                player.setLocstatecode(result.get("locstatecode").getAsString());
+            if (result.has("loccityid"))
+                player.setLoccityid(result.get("loccityid").getAsString());
 
-        player.setUpdatetime(GetNowTime.getAsString());
+            player.setUpdatetime(GetNowTime.getAsString());
+        }
+        catch (JsonIOException | JsonSyntaxException | NullPointerException e) {
+            e.printStackTrace();
+        }
 
         return player;
     }
