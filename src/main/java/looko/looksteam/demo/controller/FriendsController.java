f338a9c269ff0416dc2eba37ffc928909999663e
looko <31150545+lookoutldz@users.noreply.github.com>
2018-03-04 12:51:16 +0800

    add link and friends page

---
+++ src/main/java/looko/looksteam/demo/controller/FriendsController.java
@@ -0,0 +1,30 @@
+package looko.looksteam.demo.controller;
+
+import looko.looksteam.demo.api.GetSteamLevel;
+import looko.looksteam.demo.api.extra.GetPlayerOnlineStatus;
+import looko.looksteam.demo.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class FriendsController {
+
+    @Autowired
+    PlayerService playerService;
+
+    @RequestMapping("/friendsController")
+    public String friendsHTML(String steamid, ModelMap modelMap){
+
+        //调用service获取各种需要的数据存入modelmap
+        modelMap.addAttribute("player",playerService.selectPlayer(steamid));
+        modelMap.addAttribute("onlineStatus",new GetPlayerOnlineStatus().getStatus(steamid));
+        modelMap.addAttribute("level",new GetSteamLevel().getAsInt(steamid));
+        modelMap.addAttribute("steamid", steamid);
+
+        return "friends";
+    }
+}
